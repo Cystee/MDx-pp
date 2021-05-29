9efe7baa1deea8aa6a28ac8011c9904677e368d1
杵盐 <im@chuyn.cn>
2021-05-29 10:26:53 +0800

    Update editor.js

--- editor.js
+++ editor.js
@@ -211,7 +211,7 @@
                         }, \
                     });\
                     </script>\
-                    <script src="./MathJax-2.7.8/MathJax.js?config=TeX-AMS_HTML" async></script>');
+                    <script src="wp-content/themes/mdx/MathJax-2.7.8/MathJax.js?config=TeX-AMS_HTML" async></script>');
                 },
             });
 
