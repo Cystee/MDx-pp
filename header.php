ac48803cf0d76b821bbf2389a314ede617f1ea9b
Cystee <im@chuyn.cn>
2021-06-08 21:47:38 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,21 @@
 ﻿<!DOCTYPE html>
 <html <?php language_attributes();?>>
-<head><?php if(!empty(mdx_get_option('ga'))){?><!-- Global site tag (gtag.js) - Google Analytics -->
+<head><!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//see.cusalt.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+<?php if(!empty(mdx_get_option('ga'))){?><!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo mdx_get_option('ga');?>"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
